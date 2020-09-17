83c438d9530f5037a2c05ed980553f685064cf76
Ze Dang <4649tz@gmail.com>
2020-09-17 23:09:14 +0800

    [zedang] feat: finish Practice1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.capability.gtb;
 
+import static java.time.temporal.ChronoUnit.DAYS;
+
 import java.time.LocalDate;
+import java.time.Year;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +14,13 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    int year = date.getYear();
+    LocalDate laborDate = LocalDate.of(year, 5, 1);
+    long daysBetween = DAYS.between(date, laborDate);
+
+    if (daysBetween < 0) {
+      daysBetween += Year.of(year+1).length();
+    }
+    return daysBetween;
   }
 }
